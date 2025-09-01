bdd7a3231b364077684f0427071bf868d3226b0b
ultramarinow <ultramarinow104@gmail.com>
2025-09-01 19:11:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from aiogram import Bot
 
 
-TOKEN = '8195923726:AAESohqd8AyAPiug2_zpNyRcOLOh56PTu_s'
+TOKEN = # секрет
 DATABASE_NAME = 'vampdata.db'
 ADMIN_ID = # Замените на свой ID
 bot = Bot(TOKEN)
